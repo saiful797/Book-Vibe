a7d3e6f73ee9106ee85f8ea7efeee03aca7fb8ee
saiful797 <saifulislamsaif797@gmail.com>
2024-03-25 23:09:57 +0600

    Updated Footer  and  Home Section

---
+++ src/components/Books/Books.jsx
@@ -0,0 +1,9 @@
+const Books = () => {
+    return (
+        <div>
+            <p className="text-6xl text-center">Books</p>
+        </div>
+    );
+};
+
+export default Books;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import Books from "../Books/Books";
 
 const Home = () => {
     return (
         <div className="space-y-4">
             <Banner></Banner>
             <p className="text-6xl text-center">This is Home</p>
+            <Books></Books>
         </div>
     );
 };
