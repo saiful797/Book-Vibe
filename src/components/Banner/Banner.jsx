cbe18e7e74dd1e1783a2a787d38deb22cf400dad
saiful797 <saifulislamsaif797@gmail.com>
2024-03-25 23:20:21 +0600

    Updated Responsive Layout

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,11 @@
 
 const Banner = () => {
     return (
-        <div className="flex justify-between bg-slate-100 p-5 rounded-xl">
-            <div className="w-3/4 space-y-5">
-                <p className="text-6xl">Books to freshen up</p>
-                <p className="text-6xl">your bookshelf</p>
-                <button className="btn bg-[#23BE0A]">View The List</button>
+        <div className="flex justify-between bg-slate-100 p-16 rounded-xl mt-10">
+            <div className="w-3/4 space-y-10">
+                <p className="text-4xl md:text-6xl">Books to freshen up</p>
+                <p className="text-4xl md:text-6xl">your bookshelf</p>
+                <button className="btn bg-[#23BE0A] text-white text-xl">View The List</button>
             </div>
             <div className="flex justify-center items-center">
                 <img src="https://i.ibb.co/yF7Wry9/banner-img.png" alt="" />
