f5744220b3019a33107852f7b9fba9c80e6ba645
saiful797 <saifulislamsaif797@gmail.com>
2024-03-26 02:48:07 +0600

    Updated Footer Section and Book Cardd Section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,8 +3,8 @@ const Banner = () => {
     return (
         <div className="flex justify-between bg-slate-100 p-16 rounded-xl mt-10 mb-16">
             <div className="w-3/4 space-y-10">
-                <p className="text-4xl md:text-6xl">Books to freshen up</p>
-                <p className="text-4xl md:text-6xl">your bookshelf</p>
+                <p className="text-4xl md:text-6xl font-medium">Books to freshen up</p>
+                <p className="text-4xl md:text-6xl font-medium">your bookshelf</p>
                 <button className="btn bg-[#23BE0A] text-white text-xl">View The List</button>
             </div>
             <div className="flex justify-center items-center">
--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -5,7 +5,7 @@ const Book = ({book}) => {
     const {image,tags,bookName,author,category,rating}= book;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
-            <figure><img className='w-full h-[400px] m-5 rounded-lg' src={image} alt="Shoes" /></figure>
+            <figure className='bg-slate-300'><img className='w-[70%] h-[400px] m-5 rounded-lg' src={image} alt="Shoes" /></figure>
 
             <div className="card-body">
                 <div className='flex gap-2'>
