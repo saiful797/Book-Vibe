ff17ff48d33e685b4569222df6392102a04f3ffd
saiful797 <saifulislamsaif797@gmail.com>
2024-03-25 22:07:44 +0600

    Added Banner Section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,15 @@
+
 const Banner = () => {
     return (
-        <div>
-            <p className="text-6xl text-center">Home Page Banner</p>
+        <div className="flex justify-between bg-slate-100 p-5 rounded-xl">
+            <div className="w-3/4 space-y-5">
+                <p className="text-6xl">Books to freshen up</p>
+                <p className="text-6xl">your bookshelf</p>
+                <button className="btn bg-[#23BE0A]">View The List</button>
+            </div>
+            <div className="flex justify-center items-center">
+                <img src="https://i.ibb.co/yF7Wry9/banner-img.png" alt="" />
+            </div>
         </div>
     );
 };
