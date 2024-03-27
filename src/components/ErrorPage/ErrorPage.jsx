b50e734b0b04d84e2846717bcc73faaa39fe3c3c
saiful797 <saifulislamsaif797@gmail.com>
2024-03-27 21:19:09 +0600

    Updated Error message page and error button

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 const ErrorPage = () => {
     return (
         <div className="text-center space-y-5 mt-56">
-            <p>Oops!!! 404 Not Found</p>
+            <p>Oops!!! Error 404!</p>
+            <p>Page Not Found!</p>
             <Link className="btn bg-[#4cc03a] text-xl font-medium text-white" to="/">Back to Home</Link>
         </div>
     );
